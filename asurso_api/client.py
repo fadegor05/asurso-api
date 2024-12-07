eaf44acaa41eb427be9b0bb9f3f5b80bde02c39b
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-07 17:11:35 +0400

    refactored, separated auth data and login from account data

--- asurso_api/client.py
+++ asurso_api/client.py
@@ -8,37 +8,38 @@ from asurso_api.auth import (
 from asurso_api.context import Context
 from asurso_api.diary.diary import Diary, get_diary_info
 from asurso_api.student import get_student_info, Student
-from asurso_api.utils import md5
+from asurso_api.utils.hash import md5
 
 
 @dataclass
 class ASURSOClient:
-    login: str
-    hashed_password: str
-    password_length: int
     login_data: LoginData = None
     auth_data: AuthData = None
     student: Student = None
     context: Context = None
 
     @classmethod
-    def create(cls, login: str, password: str):
+    def from_account_data(cls, login: str, password: str):
         login_data = LoginData.get()
         hashed_password = md5(login_data.salt + md5(password))
         return cls(
-            login=login,
-            hashed_password=hashed_password,
-            password_length=len(password),
+            auth_data=AuthData.auth(login, hashed_password, len(password), login_data)
+        )
+
+    @classmethod
+    def from_auth_data(cls, auth_data: AuthData, login_data: LoginData | None = None):
+        return cls(
+            auth_data=auth_data,
             login_data=login_data,
         )
 
-    def auth(self):
-        self.auth_data = AuthData.auth(self.login, self.hashed_password, self.password_length, self.login_data)
+    def init(self):
         self.context = Context.from_auth_data(self.auth_data)
         self.student = get_student_info(self.auth_data)
 
     def logout(self):
-        self.auth_data.logout(self.login_data)
+        if self.login_data:
+            self.auth_data.logout(self.login_data)
 
-    def get_diary(self, week_start: date) -> Diary:
-        return get_diary_info(self.auth_data, self.student, week_start)
+    def get_diary(self, day: date) -> Diary:
+        return get_diary_info(self.auth_data, self.student, self.context, day)

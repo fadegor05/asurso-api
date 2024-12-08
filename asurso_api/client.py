900d5684bfbc478c0cb3f88565843bef7f9376e1
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-08 15:57:13 +0400

    refactored to pre auth data

--- asurso_api/client.py
+++ asurso_api/client.py
@@ -2,8 +2,7 @@ from dataclasses import dataclass
 from datetime import date
 
 from asurso_api.auth import (
-    AuthData,
-    LoginData,
+    AuthData, PreAuthData,
 )
 from asurso_api.context import Context
 from asurso_api.diary.diary import Diary, get_diary_info
@@ -15,25 +14,25 @@ from asurso_api.utils.hash import md5
 @dataclass
 class ASURSOClient:
     base_url: str
-    login_data: LoginData = None
+    pre_auth_data: PreAuthData = None
     auth_data: AuthData = None
     student: Student = None
     context: Context = None
 
     @classmethod
     def from_account_data(cls, login: str, password: str, region: Region = Region.SAM):
-        login_data = LoginData.get(region.value)
-        hashed_password = md5(login_data.salt + md5(password))
+        pre_auth_data = PreAuthData.get(region.value)
+        hashed_password = md5(pre_auth_data.salt + md5(password))
         return cls(
-            auth_data=AuthData.auth(region.value, login, hashed_password, len(password), login_data),
+            auth_data=AuthData.auth(region.value, login, hashed_password, len(password), pre_auth_data),
             base_url=region.value,
         )
 
     @classmethod
-    def from_auth_data(cls, auth_data: AuthData, login_data: LoginData | None = None, region: Region = Region.SAM):
+    def from_auth_data(cls, auth_data: AuthData, pre_auth_data: PreAuthData | None = None, region: Region = Region.SAM):
         return cls(
             auth_data=auth_data,
-            login_data=login_data,
+            pre_auth_data=pre_auth_data,
             base_url=region.value,
         )
 
@@ -42,8 +41,8 @@ class ASURSOClient:
         self.student = get_student_info(self.base_url, self.auth_data)
 
     def logout(self):
-        if self.login_data:
-            self.auth_data.logout(self.base_url, self.login_data)
+        if self.pre_auth_data:
+            self.auth_data.logout(self.base_url, self.pre_auth_data)
 
     def get_diary(self, day: date) -> Diary:
         return get_diary_info(self.base_url, self.auth_data, self.student, self.context, day)
